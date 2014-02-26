5a068fb076525727025f17ec8553eb3f11a63e24
sham <sham@succez.com>
2014-02-26 12:51:03 +0800

    首次

---
+++ com.sandbox.commons.util/src/main/java/com/sandbox/commons/util/StringUtils.java
@@ -0,0 +1,13 @@
+package com.sandbox.commons.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StringUtils {
+	private static final Logger log = LoggerFactory
+			.getLogger(StringUtils.class);
+	
+	public static void main(String[] args) {
+		log.debug("test");
+	}
+}
